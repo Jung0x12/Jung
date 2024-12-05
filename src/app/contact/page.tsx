37556a4420460e1064ed39d3035b66a111495a87
Jung <jwork1994212@gmail.com>
2024-12-05 23:18:56 +0800

    feat: done Contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/common/Header";
+import ContactContent from "@/components/contact/ContactContent";
+
+export default function Contact() {
+  return (
+    <div className="h-screen flex flex-col">
+      <Header />
+      <ContactContent />
+    </div>
+  );
+}
---
+++ src/components/contact/ContactContent.tsx
@@ -0,0 +1,52 @@
+const ContactContent = () => {
+  const socialLinks = [
+    { name: "Linkedin", url: "https://www.linkedin.com/in/jung0x12/" },
+    { name: "Github", url: "https://github.com/Jung0x12" },
+    { name: "X", url: "https://x.com/Tuanimoo" },
+  ];
+
+  const SocialLink = ({ name, url }: { name: string; url: string }) => (
+    <a
+      href={url}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="hover:underline mx-2"
+    >
+      {name}
+    </a>
+  );
+
+  return (
+    <div className="flex flex-grow mt-4">
+      <div className="w-[55%]">
+        <div className="flex flex-col h-full items-end justify-center">
+          <div className="w-3/5 2xl:w-3/5">
+            <div className="text-9xl font-bold">Contact</div>
+            <div className="text-xl leading-snug mt-10 ms-4">
+              Get in touch with me via social media
+              <br /> or send me an email.
+            </div>
+            <div className="text-lg mt-5 ms-4">
+              <div>
+                Email: jwork1994212@gmail.com
+                <br />
+                ENS: Jung0x12.eth
+              </div>
+              <div>
+                Social Media:
+                {socialLinks.map((link, index) => (
+                  <span key={index}>
+                    <SocialLink name={link.name} url={link.url} />
+                    {index < socialLinks.length - 1 && "/"}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContactContent;
