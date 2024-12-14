462ed32f2bb3215d18f4c38917f9b110b2a2f518
Jung <jwork1994212@gmail.com>
2024-12-15 07:00:50 +0800

    fix: update Next config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   output: "export",
   distDir: "out",
   images: { unoptimized: true },
+  assetPrefix: isProd ? '/Jung0x12.github.io/' : '',
+  basePath: isProd ? '/Jung0x12.github.io' : '',
 };
 
 export default nextConfig;
